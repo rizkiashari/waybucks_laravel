1f87b8f39e3a4db84f9d47a1cc00d3aa028a0131
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-30 22:03:27 +0700

    add jumbotron v2

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -1,3 +1,3 @@
-<div class="w-full mt-[-2em] md:mb-8 mb-5 md:mt-0 h-[300px]">
+<div class="w-full mt-[-2em] md:mb-8 mb-5 md:mt-0">
   <img src="/jumbotron/bg-jumbotron.png" class="object-cover"/>
 </div>
