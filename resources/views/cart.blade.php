61e49c2166bc6f614552e07716b3dd375f6d55ff
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-30 21:35:25 +0700

    add message error and success

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,33 @@
 @extends('layout.main')
 
 @section('content')
-    
+ @if ($errors->any())
+    <div x-data="{ open: true }" :class="{'flex': open, 'hidden': !open}" x-open="open" x-init="setTimeout(() => open = false, 3000)"  role="alert">
+      <div class="bg-[#f8d7da] border-[2px] w:-[100px] sm:w-[350px] md:w-[600px] border-[#f5c2c7] text-[#842029] px-10 py-3 rounded absolute top-[7em] left-[50%] translate-x-[-50%]">
+        <span class="absolute left-0 top-0 px-4 py-3">
+          <svg @click="open = !open" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        </span>
+        <div class="flex flex-col ml-4">
+          <strong class="font-bold text-[10px] sm:text-[12px] md:text-[16px] md:w-[400px] w-[300px]">There were {{ count($errors) }} errors with your submission</strong>
+          <ul class="mt-2">
+            @foreach ($errors->all() as $error)
+            <li class="text-[10px] sm:text-[12px] md:text-[16px]">{{ $error }}</li>
+            @endforeach
+          </ul> 
+        </div> 
+      </div>
+    </div>
+  @endif
+  @if (session()->has('success'))
+    <div x-data="{ open: true }" :class="{'flex': open, 'hidden': !open}"  x-open="open" x-init="setTimeout(() => open = false, 2500)" role="alert">
+        <div class="bg-[#d1e7dd] z-[99] border-[2px] w:-[100px] sm:w-[350px] md:w-[600px] border-[#badbcc] text-[#0f5132] px-10 py-3 rounded absolute top-[7em] left-[50%] translate-x-[-50%]">
+            <span class="absolute left-0 top-0 px-4 py-3">
+            <svg @click="open = !open" class="fill-current h-6 w-6 text-[#539b7b]" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+            <strong class="font-bold text-[10px] sm:text-[12px] md:text-[16px] md:w-[400px] w-[300px]">{{ session('success') }}</strong>     
+        </div>
+    </div>
+  @endif
 <div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
     <div class="flex gap-8 md:flex-row flex-col w-full">
         <div class="md:w-3/5 w-full">
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,29 +1,56 @@
 @extends('layout.main')
 
 @section('content')
-<div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-    <form class="mb-4" action="/register" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-6">
-            <input class="form-control" type="email" name="email" placeholder="Email">
-            @error('email')
-            <label class="block">{{ $message }}</label>
-            @enderror
+    @if ($errors->any())
+        <div x-data="{ open: true }" :class="{'flex': open, 'hidden': !open}" x-open="open" x-init="setTimeout(() => open = false, 3000)"  role="alert">
+        <div class="bg-[#f8d7da] border-[2px] w:-[100px] sm:w-[350px] md:w-[600px] border-[#f5c2c7] text-[#842029] px-10 py-3 rounded absolute top-[7em] left-[50%] translate-x-[-50%]">
+            <span class="absolute left-0 top-0 px-4 py-3">
+            <svg @click="open = !open" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+            <div class="flex flex-col ml-4">
+            <strong class="font-bold text-[10px] sm:text-[12px] md:text-[16px] md:w-[400px] w-[300px]">There were {{ count($errors) }} errors with your submission</strong>
+            <ul class="mt-2">
+                @foreach ($errors->all() as $error)
+                <li class="text-[10px] sm:text-[12px] md:text-[16px]">{{ $error }}</li>
+                @endforeach
+            </ul> 
+            </div> 
         </div>
-        <div class="mb-6">
-            <input class="form-control" type="password" name="password" placeholder="Password">
-            @error('password')
-            <label class="block">{{ $message }}</label>
-            @enderror
         </div>
-        <div class="mb-6">
-            <input class="form-control" type="text" name="fullname" placeholder="Full Name">
-            @error('fullname')
-            <label class="block">{{ $message }}</label>
-            @enderror
+    @endif
+    @if (session()->has('success'))
+        <div x-data="{ open: true }" :class="{'flex': open, 'hidden': !open}"  x-open="open" x-init="setTimeout(() => open = false, 2500)" role="alert">
+            <div class="bg-[#d1e7dd] z-[99] border-[2px] w:-[100px] sm:w-[350px] md:w-[600px] border-[#badbcc] text-[#0f5132] px-10 py-3 rounded absolute top-[7em] left-[50%] translate-x-[-50%]">
+                <span class="absolute left-0 top-0 px-4 py-3">
+                <svg @click="open = !open" class="fill-current h-6 w-6 text-[#539b7b]" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+                <strong class="font-bold text-[10px] sm:text-[12px] md:text-[16px] md:w-[400px] w-[300px]">{{ session('success') }}</strong>     
+            </div>
         </div>
-        <input class="btn btn-primary mt-3" type="submit" value="Register">
-    </form>
-    <a href="/login">Already have an account?</a>
-</div>
+    @endif
+    <div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+        <form class="mb-4" action="/register" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-6">
+                <input class="form-control" type="email" name="email" placeholder="Email">
+                @error('email')
+                <label class="block">{{ $message }}</label>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <input class="form-control" type="password" name="password" placeholder="Password">
+                @error('password')
+                <label class="block">{{ $message }}</label>
+                @enderror
+            </div>
+            <div class="mb-6">
+                <input class="form-control" type="text" name="fullname" placeholder="Full Name">
+                @error('fullname')
+                <label class="block">{{ $message }}</label>
+                @enderror
+            </div>
+            <input class="btn btn-primary mt-3" type="submit" value="Register">
+        </form>
+        <a href="/login">Already have an account?</a>
+    </div>
 @endsection
