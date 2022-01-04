327074918b00b17376be1f65a6b645635dd9c87d
RizkiAshari <rizkiashari1026@gmail.com>
2022-01-04 14:11:46 +0700

    fix layout product admin and topping admin

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -30,35 +30,37 @@
   <div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
     <div>
         @if (count($products) > 0)
-        <h1 class="md:mb-16 mb-12 font-extrabold md:text-[36px] sm:text-[24px] text-[16px] text-[#BD0707]">Product</h1>
-            <div class="flex md:gap-9 sm:gap-6 gap-4 mb-4 md:mb-8 flex-wrap">
-                @foreach ($products as $product)
-                <form class="mb-5" 
-                        action="{{url('deleteproduct/'.$product->id)}}" 
-                        method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <div class="md:w-[60%] rounded-[8px] w-[45%] relative bg-[#F6DADA]">
-                        <a href="/product/{{ $product->slug_product }}">
-                            <div class="before:absolute before:top-0 before:bottom-0 before:right-0 before:left-0 before:bg-[#3d3d3d33] before:rounded-[8px]">
-                                <img class="w-full object-cover" src="<?php echo asset("storage/images/$product->photo_product") ?>" />
-                            </div>
-                            <h3 class="text-[#bf0707] md:text-[18px] font-bold text-[12px] sm:text-[16px] px-4 py-2">{{ $product->name_product }}</h3>
-                            <p class="text-[#974A4A] px-4 pb-2 md:text-[14px] sm:text-[12px] text-[10px]">{{ $product->price_product }}</p>
-                        </a>
+          <h1 class="md:mb-16 mb-12 font-extrabold md:text-[36px] sm:text-[24px] text-[16px] text-[#BD0707]">Product</h1>
+          <div class="flex md:gap-9 sm:gap-6 gap-4 mb-4 md:mb-8 flex-wrap">
+              @foreach ($products as $product)
+                <form class="mb-5" action="{{url('deleteproduct/'.$product->id)}}" method="POST">
+                  @method('DELETE')
+                  @csrf
+                  <div class="rounded-t-[8px] relative bg-[#F6DADA]">
+                    <div class="before:absolute before:top-0 before:bottom-0 before:right-0 before:left-0 before:bg-[#3d3d3d33] before:rounded-t-[8px] ">
+                        <img class="w-[250px] object-cover" src="<?php echo asset("storage/images/$product->photo_product") ?>" />
                     </div>
-                      <div class="inline-flex rounded-[8px] w-[45%] relative bg-[#F6DADA]">
-                        <button type="submit"class="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">Delete</button>
-                        <a href="{{url('updateproduct/'.$product->id)}}" class="bg-grey-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">Update</a>
-                      </div>
-                    </form>
-                @endforeach
-            </div>
-            
+                    <h3 class="text-[#bf0707] md:text-[18px] font-bold text-[12px] sm:text-[16px] px-4 py-2">{{ $product->name_product }}</h3>
+                    <p class="text-[#974A4A] px-4 pb-2 md:text-[14px] sm:text-[12px] text-[10px]">{{ $product->price_product }}</p>
+                  </div>
+                  <div class="inline-flex w-full">
+                    <button type="submit" class="w-[50%] bg-[#fff] border-[#974A4A] rounded-bl-lg border-[1px] text-[14px] text-[#111545] font-medium py-2 px-3">Delete</button>
+                    <a href="{{url('updateproduct/'.$product->id)}}" class="w-[50%] rounded-br-lg bg-[#974A4A] text-center text-[14px] text-[#fff] font-medium py-2 px-3">Update</a>
+                  </div>         
+              </form>
+            @endforeach
+          </div>
         @else
             <p class="capitalize md:text-[32px] text-[14px] font-semibold">Product Not found</p>
         @endif
     </div>
-        <a href="{{url('addproduct')}}" style="transition:.3s; -webkit-tap-highlight-color: rgba(0,0,0,0);position: fixed; bottom: 16px;right: 16px;" class="overflow-scroll"><svg class="h-16 w-16 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="9" y1="12" x2="15" y2="12" />  <line x1="12" y1="9" x2="12" y2="15" /></svg></a>
-</div>
+    <a href="{{url('addproduct')}}" style="-webkit-tap-highlight-color: rgba(0,0,0,0);" class="transition-[0.3s] fixed bottom-4 md:right-32 sm:right-8 right-4">
+      <svg class="h-14 w-14 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <circle cx="12" cy="12" r="9" />  
+        <line x1="9" y1="12" x2="15" y2="12" />  
+        <line x1="12" y1="9" x2="12" y2="15" />
+      </svg>
+    </a>
+  </div>
 @endsection
